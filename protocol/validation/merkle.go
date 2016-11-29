580f4dd1177f5ce6611402a19968c10a432664f2
Jackson Owens <jackson@chain.com>
2016-11-29 15:31:33 -0800

    protocol/validation: calc merkle root iteratively
    Refactor CalcMerkleRoot to calculate the merkle root iteratively instead
    of recursively. Use a single large bc.Hash array and a single
    sha3.Hasher object.
    
    ```
    benchmark                     old ns/op     new ns/op     delta
    BenchmarkValidateBlock-4      201071        203009        +0.96%
    BenchmarkCalcMerkleRoot-4     47358085      45237315      -4.48%
    
    benchmark                     old allocs     new allocs     delta
    BenchmarkValidateBlock-4      111            114            +2.70%
    BenchmarkCalcMerkleRoot-4     85007          65012          -23.52%
    
    benchmark                     old bytes     new bytes     delta
    BenchmarkValidateBlock-4      6967          7449          +6.92%
    BenchmarkCalcMerkleRoot-4     2812529       2713220       -3.53%
    ```

--- protocol/validation/merkle.go
+++ protocol/validation/merkle.go
@@ -15,46 +15,65 @@ var (
 // CalcMerkleRoot creates a merkle tree from a slice of transactions
 // and returns the root hash of the tree.
 func CalcMerkleRoot(transactions []*bc.Tx) (root bc.Hash) {
-	switch {
-	case len(transactions) == 0:
+	if len(transactions) == 0 {
 		sha3pool.Sum256(root[:], nil)
 		return root
+	}
 
-	case len(transactions) == 1:
-		h := sha3pool.Get256()
-		defer sha3pool.Put256(h)
+	// The first n hashes are the leaf transaction witness hashes.
+	powOfTwo := nextPowerOfTwo(len(transactions))
+	hashes := make([]bc.Hash, powOfTwo*2-1)
+	valid := make([]bool, powOfTwo*2-1)
 
-		witHash := transactions[0].WitnessHash()
+	h := sha3pool.Get256()
+	for i, tx := range transactions {
+		witHash := tx.WitnessHash()
+
 		h.Write(leafPrefix)
 		h.Write(witHash[:])
-		h.Read(root[:])
-		return root
+		h.Read(hashes[i][:])
+		h.Reset()
 
-	default:
-		k := prevPowerOfTwo(len(transactions))
-		left := CalcMerkleRoot(transactions[:k])
-		right := CalcMerkleRoot(transactions[k:])
-
-		h := sha3pool.Get256()
-		defer sha3pool.Put256(h)
-		h.Write(interiorPrefix)
-		h.Write(left[:])
-		h.Write(right[:])
-		h.Read(root[:])
-		return root
+		valid[i] = true
 	}
+
+	off := powOfTwo
+	for i := 0; i < len(hashes)-1; i += 2 {
+		switch {
+		// If left node is invalid, the parent is invalid too.
+		case !valid[i]:
+			break
+
+		// If there is no right child, the parent is just the left child's hash.
+		case !valid[i+1]:
+			hashes[off] = hashes[i]
+			valid[off] = true
+
+		// If there are both left and right children, compute the hash of the
+		// interior prefix and both child hashes.
+		default:
+			h.Write(interiorPrefix)
+			h.Write(hashes[i][:])
+			h.Write(hashes[i+1][:])
+			h.Read(hashes[off][:])
+			h.Reset()
+			valid[off] = true
+		}
+		off++
+	}
+	return hashes[len(hashes)-1]
 }
 
-// prevPowerOfTwo returns the largest power of two that is smaller than a given number.
-// In other words, for some input n, the prevPowerOfTwo k is a power of two such that
-// k < n <= 2k. This is a helper function used during the calculation of a merkle tree.
-func prevPowerOfTwo(n int) int {
-	// If the number is a power of two, divide it by 2 and return.
+// nextPowerOfTwo returns the largest power of two that is greater than or
+// equal to  a given number. In other words, for some input n, the
+// prevPowerOfTwo k is a power of two such that k/2 < n <= k. This is a
+// helper function used during the calculation of a merkle tree.
+func nextPowerOfTwo(n int) int {
 	if n&(n-1) == 0 {
-		return n / 2
+		return n // already power of 2
 	}
 
-	// Otherwise, find the previous PoT.
-	exponent := uint(math.Log2(float64(n)))
+	// Otherwise, find the next power of two.
+	exponent := uint(math.Log2(float64(n))) + 1
 	return 1 << exponent // 2^exponent
 }
